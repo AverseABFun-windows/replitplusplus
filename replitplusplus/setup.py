21c04b80dab916efcccfe260bbc0650fbcca7297
AverseABFun <81586710+AverseABFun-windows@users.noreply.github.com>
2023-01-17 21:55:20 +0000

    stuff

--- replitplusplus/setup.py
+++ replitplusplus/setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
 with open("replitplusplus/README.md", "r") as fhandle:
-    long_description = fhandle.read() # Your README.md file will be used as the long description!
+    long_description = fhandle.read()
 
 setuptools.setup(
     name="replitplusplus",
-    version="0.0.1", # The version of your package!
-    author="AverseABFun", # Your name here!
-    author_email="averse.abfun@gmail.com", # Your e-mail here!
-    description="The replit package, but better!", # A short description here!
+    version="1.0",
+    author="AverseABFun",
+    author_email="averse.abfun@gmail.com",
+    description="The replit package, but better!",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AverseABFun-windows/replitplusplus", # Link your package website here! (most commonly a GitHub repo)
-    packages=setuptools.find_packages(), # A list of all packages for Python to distribute!
+    url="https://github.com/AverseABFun-windows/replitplusplus",
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ], # Enter meta data into the classifiers list!
-    python_requires='>=3.6', # The version requirement for Python to run your package!
+    ],
+    python_requires='>=3.6',
 )
